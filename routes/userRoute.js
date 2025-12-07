b9db9f8ee49e23b817f42fd8def0fe5c05690f36
richhmindxai <richhmindxai@gmail.com>
2025-12-07 16:23:53 +0530

    new

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,31 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 const upload = require("../middleware/uploadMiddleware"); // your image upload middleware
-const { verifyAdminToken } = require("");
+const { verifyAdminToken } = require("../middleware/verifyToken");
 const userController = require("../controllers/userController");
 
 // REGISTER
 router.post(
   "/register",
   upload.single("profilePic"),
+  verifyAdminToken,
   userController.registerUser
 );
 
 // LOGIN
-router.post("/login", userController.loginUser);
+router.post("/login", verifyAdminToken, userController.loginUser);
 
 // Create user (form-data + image upload)
 router.post(
   "/create",
   upload.single("journalImage"),
+  verifyAdminToken,
   userController.createUser
 );
 
 // Get all users
-router.get("/", userController.getUsers);
+router.get("/", verifyAdminToken, userController.getUsers);
 
 // Get single user
-router.get("/:id", userController.getUserById);
+router.get("/:id", verifyAdminToken, userController.getUserById);
 
 // Update user (with optional new image)
 router.put(
@@ -36,6 +38,6 @@ router.put(
 );
 
 // Delete user
-router.delete("/delete/:id", userController.deleteUser);
+router.delete("/delete/:id", verifyAdminToken, userController.deleteUser);
 
 module.exports = router;
